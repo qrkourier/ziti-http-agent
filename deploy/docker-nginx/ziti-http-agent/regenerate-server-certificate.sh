2705923a3ad665f6eb53f54568eff0963226aecc
qrkourier <w@qrk.us>
2022-11-26 15:17:54 +0000

    send dummy DN params for self-signed cert script

--- deploy/docker-nginx/ziti-http-agent/regenerate-server-certificate.sh
+++ deploy/docker-nginx/ziti-http-agent/regenerate-server-certificate.sh
@@ -1,4 +1,16 @@
 #/usr/bin/env bash
 set -e -u -o pipefail
 # install a self-signed cert in the current dir with the same filenames expected by nginx.conf
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./privkey.pem -out ./fullchain.pem
+
+openssl req \
+    -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout ./privkey.pem \
+    -out ./fullchain.pem <<DN
+${DN_COUNTRY:-"US"}
+${DN_STATE:-"DC"}
+${DN_CITY:-"Washington"}
+${DN_ORG:-"Acme Ltd"}
+${DN_ORG_UNIT:-"."}
+${DN_CN:-"ziti-http-agent.localdomain"}
+${DN_EMAIL:-"."}
+DN
